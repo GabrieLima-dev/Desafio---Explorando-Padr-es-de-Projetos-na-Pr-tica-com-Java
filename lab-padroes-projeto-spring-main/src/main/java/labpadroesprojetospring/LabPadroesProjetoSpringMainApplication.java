be3a6dfe331b9a2a23fad028095e7710737f0770
Gabriellima-dev <gabrieldesouzalima@gmail.com>
2023-10-19 14:41:04 -0300

    Project Finish

---
+++ lab-padroes-projeto-spring-main/src/main/java/labpadroesprojetospring/LabPadroesProjetoSpringMainApplication.java
@@ -0,0 +1,13 @@
+package labpadroesprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesProjetoSpringMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringMainApplication.class, args);
+	}
+
+}
